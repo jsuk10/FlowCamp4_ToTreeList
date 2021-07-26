84a85a343a70b09cefdd7742e5a39dd806305d27
jsuk10 <jsuk970407@gmail.com>
2021-07-26 15:03:06 +0900

    feat : Add Test

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:calendar_timeline/calendar_timeline.dart';
 import "package:flutter_localizations/flutter_localizations.dart";
 import 'calendar.dart';
+import 'src/RandomWord.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,21 +12,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [
-        const Locale('ko', 'KR'),  
-        const Locale('en', 'US')  
-      ],
-      home: Calendar(),
-    );
+    return MaterialApp(home: RandomWords());
   }
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     title: 'Flutter Demo',
+  //     theme: ThemeData(
+  //       primarySwatch: Colors.blue,
+  //     ),
+  //     localizationsDelegates: [
+  //       GlobalMaterialLocalizations.delegate,
+  //       GlobalWidgetsLocalizations.delegate,
+  //       GlobalCupertinoLocalizations.delegate,
+  //     ],
+  //     supportedLocales: [
+  //       const Locale('ko', 'KR'),
+  //       const Locale('en', 'US')
+  //     ],
+  //     home: Calendar(),
+  //   );
+  // }
 }
---
+++ lib/src/RandomWord.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'toDoList.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => RandomWordState();
+}
